5db8153ec3acaaa4c6dfbe8b8d7064947089f2ba
Brage <5640782+2xic@users.noreply.github.com>
2023-09-28 09:37:09 +0200

    Allow external construction of the storage slot hashes (#99)
    # Summary
    
    Adds support for external construction of the storage slot hashes
    
    # Details 
    - Adds a new method to `StorageSlotHashes` so it can be constructed with a predefined BitMap of the hashes. 
    - Adds a new method to `LiftingPasses` so it can be constructed with a vector of passes 
    
    # Checklist
    
    - [x] Code is formatted by Rustfmt.
    - [x] Documentation has been updated if necessary.
    
    Fixes #97

--- src/inference/lift/mod.rs
+++ src/inference/lift/mod.rs
@@ -66,6 +66,14 @@ pub struct LiftingPasses {
 }
 
 impl LiftingPasses {
+    /// Creates a new instance of the lifting pass with the provided `passes`
+    #[must_use]
+    pub fn new(passes: impl Into<Vec<Box<dyn Lift>>>) -> Self {
+        Self {
+            passes: passes.into(),
+        }
+    }
+
     /// Adds the `pass` to the end of the pass ordering.
     ///
     /// If a pass of the given type already exists in the ordering, it will not
